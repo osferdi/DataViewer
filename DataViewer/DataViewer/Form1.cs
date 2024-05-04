1e8aa6694d4eddbda373869d09374a1e00519de3
oscar <oscar_fernandez_diaz@hotmail.com>
2024-05-04 16:37:39 +0200

    Initial form creation test

---
+++ DataViewer/DataViewer/Form1.cs
@@ -0,0 +1,12 @@
+namespace DataViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Data Viewer";
+        }
+    }
+}
